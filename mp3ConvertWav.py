1bc2df414fc31881de848c0ccd270135ff2a768b
SAQLAIN-MUSHTAQUE <64137226+SAQLAIN-MUSHTAQUE@users.noreply.github.com>
2022-09-21 00:16:47 +0500

    All files

---
+++ mp3ConvertWav.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# importingm all liabraries that we need
+
+import os
+from pydub import AudioSegment
+import glob
+
+
+# In[5]:
+
+
+# creating class
+
+class con:
+    def convert (data_file):
+        
+        # importing data
+        path = (data_file)
+
+        #Change working directory
+        os.chdir(path)
+
+        # creatig list
+        audio_files = os.listdir()
+
+        for file in audio_files:
+            #spliting the file into the name and the extension
+            wav = os.path.splitext(file)[0]+ '.wav'
+            mp3_sound = AudioSegment.from_mp3(file)
+
+            #rename them using the old name + ".wav"
+            mp3_sound.export(wav, format="wav")
+        print ("mp3 converted into wav");
+
+    def cut (data_file):
+        
+        # importing data
+        path = (data_file)
+
+        #Change working directory
+        os.chdir(path)
+
+        # creatig list
+        audio_files = os.listdir()
+
+        for file in audio_files:
+            #spliting the file into the name and the extension
+            wav = os.path.splitext(file)[0]+ '.wav'
+            mp3_sound = AudioSegment.from_mp3(file)
+
+            #rename them using the old name + ".wav"
+            mp3_sound.export(wav, format="wav")
+            os.remove(file)
+        print ("mp3 converted and removed mp3 into wav");
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
